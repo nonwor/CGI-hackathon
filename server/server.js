e9619afe49a3a4768bf3050a4bd5b304ee14d8ea
Louie <sxl7953@rit.edu>
2023-10-19 16:19:47 -0400

    finished schemas

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const PORT = process.env.PORT || 3000
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
-    res.send('You have reached the catch all route!')
-})
-
+app.get("*", (req, res) => {
+  res.send("You have reached the catch all route!");
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
