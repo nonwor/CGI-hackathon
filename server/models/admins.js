9a99551a12a36ebd9e16f563982f4675a8203288
Louie <sxl7953@rit.edu>
2023-10-19 16:04:26 -0400

    added user and admin schemas

--- server/models/admins.js
+++ server/models/admins.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
+  email: { type: String, required, trim },
+  password: { type: String, required, minlength: 3 },
+});
 
-})
-
-
-module.exports = mongoose.model('Admin', adminSchema);
+module.exports = mongoose.model("Admin", adminSchema);
--- server/models/users.js
+++ server/models/users.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  firstName: { type: String, required, trim },
+  lastName: { type: String, required, trim },
+  email: { type: String, required, trim },
+  age: { type: Age, required, trim },
+  location: { type: String, required },
+});
 
-})
-
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
