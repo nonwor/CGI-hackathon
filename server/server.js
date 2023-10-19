bdb44072b99ccfac4b54951519407afcc3d9a78e
Louie <sxl7953@rit.edu>
2023-10-19 17:24:38 -0400

    fixed db error

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const PORT = process.env.PORT || 3000
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
-    res.send('You have reached the catch all route!')
-})
+const ConnectoDB = require("./config/db");
 
+ConnectoDB();
+
+app.get("*", (req, res) => {
+  res.send("You have reached the catch all route!");
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
