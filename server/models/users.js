2b444d7b19f393bfae552b12d1bb0e1df81182bf
Louie <sxl7953@rit.edu>
2023-10-19 17:23:12 -0400

    finished userschema

--- server/models/users.js
+++ server/models/users.js
@@ -4,10 +4,10 @@ const { Schema } = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required, trim },
   lastName: { type: String, required, trim },
-  email: { type: String, required, trim },
-  age: { type: Number, required, trim },
-  location: { type: String, required },
-  userId: { type: Schema.Types.ObjectId },
+  email: { type: String, required, trim, unique },
+  morethan18: { type: Boolean, required },
+  highschool: { type: Boolean, required },
+  commitment: { type: Boolean, required },
 });
 
 module.exports = mongoose.model("User", userSchema);
--- server/server.cjs
+++ server/server.cjs
@@ -3,6 +3,10 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
+const ConnectoDB = require("./config/db");
+
+ConnectoDB();
+
 app.get("*", (req, res) => {
   res.send("You have reached the catch all route!");
 });
