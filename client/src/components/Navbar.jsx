3389accf65d831b9252569a5d0d93a1febfa1fb5
jonathanpenaloa <jona_pena_91@outlook.com>
2023-10-19 12:08:19 -0700

    created a nav manu and added styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,1 +1,13 @@
-// Navbar component
+import React from "react";
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div className="nav-bar">
+        <a href="">home</a>
+        <a href="">register</a>
+        <a href="">login</a>
+        <a href="">admin</a>
+    </div>
+  );
+}
