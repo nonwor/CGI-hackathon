2dba6229a9ef62cd700f7eac515897b20e3e739b
Louie <sxl7953@rit.edu>
2023-10-19 18:57:39 -0400

    forgot to changed email schema

--- server/models/admins.js
+++ server/models/admins.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const adminSchema = new mongoose.Schema({
   firstName: { type: String, required: true, trim: true },
   lastName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: /.+@.+\.\w+/,
+  },
   password: { type: String, required: true, minLength: 3 },
 });
 
